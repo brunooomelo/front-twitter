f3214a4a9dc182989273c0dd8151750e706bb629
bruno.melo <bruno.melo@tbfg.com.br>
2019-04-16 23:44:33 -0300

    component tweet

---
+++ src/components/Tweet.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import like from "../like.svg";
+import api from "../services/api";
+
+const Wrapper = styled.li`
+  padding: 20px 20px 0;
+  margin: 20px 0 0;
+  border-top: 1px solid #eee;
+  &:first-child {
+    margin-top: 0;
+    padding-top: 0;
+    border: 0;
+  }
+`;
+
+const Content = styled.p`
+  margin: 15px 0;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+const Button = styled.button`
+  border: 0;
+  background: transparent;
+
+  display: flex;
+  align-items: center;
+  color: #697882;
+  cursor: pointer;
+  &::hover {
+    opacity: 0.7;
+  }
+`;
+
+const Image = styled.img`
+  margin-right: 5px;
+`;
+
+function Tweet({ tweet }) {
+  async function handleLike() {
+    const { _id } = tweet;
+    await api.post(`/likes/${_id}`);
+  }
+  return (
+    <Wrapper>
+      <strong>{tweet.author.name}</strong>
+      <Content>{tweet.content}</Content>
+      <Button type="button" onClick={handleLike}>
+        <Image src={like} alt="Like" />
+        {tweet.likes}
+      </Button>
+    </Wrapper>
+  );
+}
+
+export default Tweet;
