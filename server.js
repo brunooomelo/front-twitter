5323f57b0c38150577a7941a486602ae4168683d
bruno.melo <bruno.melo@tbfg.com.br>
2019-04-17 00:23:45 -0300

    [fix] PORT

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+const port = process.env.PORT
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/ping", function(req, res) {
+  return res.status(200).json({ ping: "pong" });
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(port);
+app.listen(port, () => console.log("🤘 server up 🤘"));
